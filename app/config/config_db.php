31d06f47f58416ace6873b862a1309a1097ff60d
nesho84 <neshat84@hotmail.com>
2020-11-25 22:07:45 +0100

    Administration finished

--- app/config/config_db.php
+++ app/config/config_db.php
@@ -5,10 +5,3 @@ define('DB_NAME', 'renova_dl_db');
 define('DB_CHARSET', 'utf8');
 define('DB_USER', 'root');
 define('DB_PASS', '123456');
-
-// // Production configuration
-// define('DB_HOST', 'w01b450e.kasserver.com');
-// define('DB_NAME', 'd03467e5');
-// define('DB_CHARSET', 'utf8');
-// define('DB_USER', 'd03467e5');
-// define('DB_PASS', 'Renova#00112233');
